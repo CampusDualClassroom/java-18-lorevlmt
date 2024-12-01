159d001e4b4141d06c595c65fb555472de32088a
lorevlmt <lore.vimontes@gmail.com>
2024-12-01 16:56:43 +0100

    Ejercicio_18 Resuelto

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -1,8 +1,40 @@
 package com.campusdual.classroom;
 
+import java.util.Scanner;
+
 public class Exercise18 {
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Introducir el tamaño del array con un número entero:");
+
+		int entero = scanner.nextInt();
+
+		scanner.close();
+
+		int[] array = createAndInitializeArray(entero);
+
+		showInlineArray(array);
 	}
+
+	public static int[] createAndInitializeArray(int entero) {
+
+		int[] array = new int[entero];
+		for (int i = 0; i < entero; i++) {
+			array[i] = i + 1;
+		}
+		return array;
+	}
+
+	public static void showInlineArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+
+			if (i < array.length - 1) {
+				System.out.print(" ");
+			}
+		}
+	}
+
 }
