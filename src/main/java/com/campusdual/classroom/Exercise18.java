5135c984e217f71f5a01bdc061d6cbed74c4d27d
lorevlmt <lore.vimontes@gmail.com>
2025-01-17 14:14:09 +0100

    Ejercicio_18 Corregido: optimización de main e modificación de createAndInitializeArray() para tener en cuenta los números negativos

--- src/main/java/com/campusdual/classroom/Exercise18.java
+++ src/main/java/com/campusdual/classroom/Exercise18.java
@@ -2,26 +2,33 @@ package com.campusdual.classroom;
 
 import com.campusdual.util.Utils;
 
-import java.util.Scanner;
-
 public class Exercise18 {
 
 	public static void main(String[] args) {
 
-		int size = Utils.integer("Introducir el tamaño del array con un número entero:");
+		//int size = Utils.integer("Introducir el tamaño del array con un número entero:");
 
-		int[] array = createAndInitializeArray(size);
+		//int[] array = createAndInitializeArray(size);
 
-		showInlineArray(array);
+		//showInlineArray(array);
+
+		//correción
+		showInlineArray(createAndInitializeArray(Utils.integer("Introducir el tamaño del array con un número entero: ")));
 	}
 
-	public static int[] createAndInitializeArray(int size) {
+	public static int[] createAndInitializeArray(int size){
+	//correción: números negativos
+		if (size < 0) {
+			return new int[0];
 
-		int[] array = new int[size];
-		for (int i = 0; i < size; i++) {
-			array[i] = i + 1;
+		} else {
+			int[] array = new int[size];
+			for (int i = 0; i < array.length; i++) {	//corregido i < size
+				array[i] = i + 1;
+			}
+			return array;
 		}
-		return array;
+
 	}
 
 	public static void showInlineArray(int[] array) {
